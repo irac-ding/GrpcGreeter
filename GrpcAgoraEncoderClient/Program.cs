7f4df4dd7b65a50b7fdfc4be4f0997b1eec1594f
irac <iracding@tvunetworks.com>
2020-12-10 13:22:24 +0800

    update

---
+++ GrpcAgoraEncoderClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using TVU.AgoraEncoder.Contract;
+
+namespace GrpcAgoraEncoderClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+           // This switch must be set before creating the GrpcChannel/ HttpClient.
+           AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            
+            var channel = GrpcChannel.ForAddress("http://10.12.23.123:56790");
+            var Client = new AgoraEncoderMaster.AgoraEncoderMasterClient(channel);
+
+            AsyncServerStreamingCall<ServerMessage> serverMessageReply =  Client.Register(new ClientInfo { ClientId = "irac" });
+            while (await serverMessageReply.ResponseStream.MoveNext())
+            {
+                Console.WriteLine(serverMessageReply.ResponseStream.Current.Data);
+            }
+
+            Client.ReportEncoder(new EncoderMessage { ClientId="irac",Timestamp=1234});
+            Client.ReportEncodingStatus(new EncodingStatusMessage { ClientId = "irac", Data = "test" });
+            Console.ReadKey();
+        }
+    }
+}
