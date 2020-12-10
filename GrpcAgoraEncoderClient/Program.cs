36d333beb1fd3e54fe935c37936771a864a59146
irac <iracding@tvunetworks.com>
2020-12-10 13:41:23 +0800

    Update Program.cs

--- GrpcAgoraEncoderClient/Program.cs
+++ GrpcAgoraEncoderClient/Program.cs
@@ -10,21 +10,22 @@ namespace GrpcAgoraEncoderClient
     {
         static async Task Main(string[] args)
         {
-           // This switch must be set before creating the GrpcChannel/ HttpClient.
-           AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
-            
+            // This switch must be set before creating the GrpcChannel/ HttpClient.
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
             var channel = GrpcChannel.ForAddress("http://127.0.0.1:56790");
             var Client = new AgoraEncoderMaster.AgoraEncoderMasterClient(channel);
 
-            AsyncServerStreamingCall<ServerMessage> serverMessageReply =  Client.Register(new ClientInfo { ClientId = "irac" });
+            for (int i = 0; i <= 10; i++)
+            {
+                GenericResult genericResult1 = Client.ReportEncoder(new EncoderMessage { ClientId = "irac", Timestamp = 1234 });
+                GenericResult genericResult2 = Client.ReportEncodingStatus(new EncodingStatusMessage { ClientId = "irac", Data = i.ToString() + "test" });
+            }
+            AsyncServerStreamingCall<ServerMessage> serverMessageReply = Client.Register(new ClientInfo { ClientId = "irac" });
             while (await serverMessageReply.ResponseStream.MoveNext())
             {
                 Console.WriteLine(serverMessageReply.ResponseStream.Current.Data);
             }
-
-            Client.ReportEncoder(new EncoderMessage { ClientId="irac",Timestamp=1234});
-            Client.ReportEncodingStatus(new EncodingStatusMessage { ClientId = "irac", Data = "test" });
-            Console.ReadKey();
         }
     }
 }
